63a3f7f19032eecdd2a062b34351fd0fb3a64e7c
aotasso <popomadeoide@gmail.com>
2019-03-21 23:02:21 +0900

    a

--- dfu-util.js
+++ dfu-util.js
@@ -652,7 +652,7 @@ var device = null;
         updateButton.addEventListener('click', function() {
             console.log('click');
             firmwareFile = null;
-            fetch('https://cdn.shopify.com/s/files/1/2681/3762/files/ParipiDestroyer_v1-1b.NUCLEO_L433RC_P-17.bin?1').then(function(response) {
+            fetch('https://cdn.shopify.com/s/files/1/2681/3762/files/ParipiDestroyer_v1-1b.NUCLEO_L433RC_P-17.bin').then(function(response) {
                 return response.blob();
               }).then(function(blob) {
                 console.log(blob);
